03cbe78a4efa203e7f2133fc63fef94a4d0c659b
Igor Marques <igormarquessilva@gmail.com>
2015-08-12 21:50:25 -0600

    Add seed and improve index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root to: 'docs#index'
+
   resources :docs
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+Doc.create(title: "How to export a page in pdf",
+           description: "This tutorial will teach you how to [...]",
+           implementation: lorem);
+
+Doc.create(title: "How to use devise to send password recovery emails",
+           description: "This tutorial will teach you how to [...]",
+           implementation: lorem);
+
+Doc.create(title: "How to test models with rspec",
+           description: "This tutorial will teach you how to [...]",
+           implementation: lorem);
+
+Doc.create(title: "How to use translations in rails",
+           description: "This tutorial will teach you how to [...]",
+           implementation: lorem);
