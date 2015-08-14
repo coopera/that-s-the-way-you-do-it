4d64dd4e8daf0978bd769a5a3b276bcda39d8d5f
Igor Marques <igormarquessilva@gmail.com>
2015-08-13 22:14:10 -0600

    Get content pasted on text area

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,17 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require_self
+
+document.addEventListener("DOMContentLoaded", function () {
+  var doc_implementation = document.getElementById('doc_implementation');
+
+  doc_implementation.onpaste = function(e) {
+    var pastedText = undefined;
+    if (window.clipboardData && window.clipboardData.getData) { // to work on IE
+      pastedText = window.clipboardData.getData('Text');
+    } else if (e.clipboardData && e.clipboardData.getData) {
+      pastedText = e.clipboardData.getData('text/plain');
+    }
+  };
+});
