5e92a27b88ff46e03f7f902a3fa33f76fa26468b
Igor Marques <igormarquessilva@gmail.com>
2015-10-02 13:51:00 -0300

    Add seed

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 20151002160127) do
     t.string   "title"
     t.text     "description"
     t.text     "implementation"
+    t.integer  "user_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
