abec5909d553cad9dc6d64cdd9b8b99e0e4ada2f
Igor Marques <igormarquessilva@gmail.com>
2015-10-02 17:41:17 -0300

    WTF??????

--- spec/views/docs/edit.html.erb_spec.rb
+++ spec/views/docs/edit.html.erb_spec.rb
@@ -6,9 +6,6 @@ RSpec.describe "docs/edit", type: :view do
   end
 
   it "renders the edit doc form" do
-
-    allow(DocsController).to receive(:logged_in?).and_return(false)
-
     render
 
     assert_select "form[action=?][method=?]", doc_path(@doc), "post" do
