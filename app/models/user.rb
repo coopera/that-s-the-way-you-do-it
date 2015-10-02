f55d1de7a92534b531502c6e6f35a2c466b0724b
Igor Marques <igormarquessilva@gmail.com>
2015-10-02 13:47:25 -0300

    Gets more user info from github

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ActiveRecord::Base
   def self.create_from_omniauth(auth_hash)
     self.create(provider: auth_hash[:provider],
                 uid: auth_hash[:uid],
-                name: auth_hash[:info][:name])
+                name: auth_hash[:info][:name],
+                email: auth_hash[:info][:email],
+                avatar: auth_hash[:info][:image],
+                token: auth_hash[:credentials][:token],
+                nickname: auth_hash[:info][:nickname]
+               )
   end
 
 end
